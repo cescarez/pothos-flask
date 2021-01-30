f732f0e7742d12351951c902c41456e49fe1397f
Christabel Sebastian <christabel.escarez@gmail.com>
2021-01-30 10:18:45 -0800

    successfully posted data with a view method

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from flask import Flask
-from pyrebase import pyrebase
-import pyrebase
 import os
+from flask import Flask
+import pyrebase
 from dotenv import load_dotenv
 load_dotenv()
 
 config = {
-  'apiKey': os.getenv('FLASK_APP_FIREBASE_API_KEY'),
-  'authDomain': os.getenv('FLASK_APP_FIREBASE_AUTH_DOMAIN'),
-  'databaseURL': os.getenv('FLASK_APP_FIREBASE_DATABASE_URL'),
-  'storageBucket': os.getenv('FLASK_APP_FIREBASE_STORAGE_BUCKET'),
-#   'serviceAccount': os.getenv('')
+    'apiKey': os.getenv('FLASK_APP_FIREBASE_API_KEY'),
+    'authDomain': os.getenv('FLASK_APP_FIREBASE_AUTH_DOMAIN'),
+    # 'databaseURL': os.getenv('FLASK_APP_FIREBASE_DATABASE_URL'),
+    'databaseURL': 'https://pothos-development-default-rtdb.firebaseio.com',
+    'storageBucket': os.getenv('FLASK_APP_FIREBASE_STORAGE_BUCKET'),
+#     'serviceAccount': os.getenv('')
 }
 
 app = Flask(__name__)
@@ -18,7 +18,17 @@ firebase = pyrebase.initialize_app(config)
 
 @app.route('/hello', methods=["GET"])
 def hello():
-    return ({'message':'hello world'})
+    db = firebase.database()
+    data = {
+        'username': 'Some Test Username',
+        'name': 'Test one',
+        'phone_number': '12345678',
+        'email': 'testone@test.test'
+    }
+    db.child('users').push(data)
+    return({'message':'test data successfully posted. check database for posted data'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    print('This file has been run as main')
+else:
+    print('Error, this file should not be imported as a module.')
