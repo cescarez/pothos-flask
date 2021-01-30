dfb1f30fe4a2c20d642360add93fba9b67d0e76b
Christabel Sebastian <christabel.escarez@gmail.com>
2021-01-29 17:54:40 -0800

    imported pyrebase 4 and added config info, pulled from a .env file

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from flask import Flask
-# from pyrebase import pyrebase
-# from . import config
+from pyrebase import pyrebase
+import pyrebase
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+config = {
+  'apiKey': os.getenv('FLASK_APP_FIREBASE_API_KEY'),
+  'authDomain': os.getenv('FLASK_APP_FIREBASE_AUTH_DOMAIN'),
+  'databaseURL': os.getenv('FLASK_APP_FIREBASE_DATABASE_URL'),
+  'storageBucket': os.getenv('FLASK_APP_FIREBASE_STORAGE_BUCKET'),
+#   'serviceAccount': os.getenv('')
+}
 
 app = Flask(__name__)
-# firebase = pyrebase.initialize_app(config)
+firebase = pyrebase.initialize_app(config)
 
 @app.route('/hello', methods=["GET"])
 def hello():
-    return ({'message':'hello'})
+    return ({'message':'hello world'})
 
 if __name__ == '__main__':
     app.run(debug=True)
