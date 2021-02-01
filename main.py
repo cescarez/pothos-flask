73b32ce19ba6335d066659d776949d03c966da6c
Christabel Sebastian <christabel.escarez@gmail.com>
2021-02-01 15:12:49 -0800

    changed end points -- posts to go /users and gets to go to /owners or /sitters

--- main.py
+++ main.py
@@ -16,57 +16,58 @@ config = {
 app = Flask(__name__)
 firebase = pyrebase.initialize_app(config)
 
-@app.route('/<string:usertype>', methods=['GET', 'POST'])
-def users_index(usertype):
+@app.route('/users', methods=['POST'])
+def add_user():
     db = firebase.database()
-    usertype = escape(usertype)
     if request.method == 'POST':
         #assumes JSON format, not form 
         submitted_data = request.get_json()
-        if (usertype == 'sitters' or usertype == 'owners'):
-            new_user = {
-                'date_joined': str(datetime.utcnow()),
-                #how will this be passed from front end?
-                #run posts to check if evaluates
-                'sitter': (usertype == 'sitters'),
-                'owner': (usertype == 'owners'),
-                'username': submitted_data['username'],
-                'full_name': submitted_data['full_name'],
-                'phone_number': submitted_data['phone_number'],
-                'email': submitted_data['email'],
-                'address': {
-                    'street': submitted_data['address']['street'],
-                    'city': submitted_data['address']['city'],
-                    'state': submitted_data['address']['state'],
-                    'postal_code': submitted_data['address']['postal_code'],
-                    'country': submitted_data['address']['country']
-                },
-                'avatar_url': submitted_data['avatar_url'],
-                # 'rating': submitted_data['rating'],
-                'messages': {},
-                'price_rate': {
-                    'water_by_plant': submitted_data['price_rate']['water_by_plant'],
-                    'water_by_time': submitted_data['price_rate']['water_by_time'],
-                    'repot_by_plant': submitted_data['price_rate']['repot_by_plant'],
-                    'repot_by_time': submitted_data['price_rate']['repot_by_time']
-                } 
+        new_user = {
+            'date_joined': str(datetime.utcnow()),
+            'sitter': submitted_data['sitter'],
+            'owner': submitted_data['owner'],
+            'username': submitted_data['username'],
+            'full_name': submitted_data['full_name'],
+            'phone_number': submitted_data['phone_number'],
+            'email': submitted_data['email'],
+            'address': {
+                'street': submitted_data['address']['street'],
+                'city': submitted_data['address']['city'],
+                'state': submitted_data['address']['state'],
+                'postal_code': submitted_data['address']['postal_code'],
+                'country': submitted_data['address']['country']
+            },
+            'avatar_url': submitted_data['avatar_url'],
+            # 'rating': submitted_data['rating'],
+            'messages': {},
+            'price_rate': {
+                'water_by_plant': submitted_data['price_rate']['water_by_plant'],
+                'water_by_time': submitted_data['price_rate']['water_by_time'],
+                'repot_by_plant': submitted_data['price_rate']['repot_by_plant'],
+                'repot_by_time': submitted_data['price_rate']['repot_by_time']
             }
-            
-            db.child('users').push(new_user)
-            return({'message':'new user successfully added. check database for posted data'})
-        else:
-            return({'message':'invalid endpoint. No user created.'})
+        }
+        db.child('users').push(new_user)
+        return({'message':'new user successfully added. check database for posted data'})
     else:
-        #does db order change when a patch request is sent? if so, then chain a .order_by_key() as first call
-        if usertype == 'sitters':
+        return({'message':'invalid endpoint. No user created.'})
+
+@app.route('/<string:usertype>', methods=['GET'])
+def users_index(usertype):
+    #does db order change when a patch request is sent? if so, then chain a .order_by_key() as first call
+    usertype = escape(usertype)[0:-1]
+    db = firebase.database()
+    if (usertype == 'sitter' or usertype == 'owner'):
+        if usertype == 'sitter':
             users = db.child('users').order_by_child('sitter').equal_to(True).get().val()
         else:
-            users = db.child('users').order_by_child('owner').equal_to(True).get().val()
-
+            users = db.child('users').order_by_child('sitter').equal_to(True).get().val()
         if users:
             return(users)
         else:
             return({'message': 'No users'})
+    else:
+        return({'message':'invalid endpoint.'})
 
 @app.route('/<string:usertype>/<string:id>', methods=['GET'])
 def users_show(usertype, id):
